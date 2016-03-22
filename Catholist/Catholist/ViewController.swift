1a1bc738f9e4abdccea02ea63c83daba658bc56d
Hoan Tran <hoan@bluepego.com>
2016-03-22 01:49:48 -0700

    First commit: initial FB login

---
+++ Catholist/Catholist/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Catholist
+//
+//  Created by Hoan Tran on 3/12/16.
+//  Copyright © 2016 Pego Consulting. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    @IBOutlet weak var btnFacebook: FBSDKLoginButton!
+    @IBOutlet weak var ivUserProfileImage: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureFacebook()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //    MARK: FBSDKLoginButtonDelegate Methods
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!)
+    {
+        FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":"first_name, last_name, picture.type(large)"]).startWithCompletionHandler { (connection, result, error) -> Void in
+            
+            let strFirstName: String = (result.objectForKey("first_name") as? String)!
+            let strLastName: String = (result.objectForKey("last_name") as? String)!
+            let strPictureURL: String = (result.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as? String)!
+            
+            self.lblName.text = "Welcome, \(strFirstName) \(strLastName)"
+            self.ivUserProfileImage.image = UIImage(data: NSData(contentsOfURL: NSURL(string: strPictureURL)!)!)
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!)
+    {
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logOut()
+        
+        ivUserProfileImage.image = nil
+        lblName.text = ""
+    }
+    
+    //    MARK: Other Methods
+    
+    func configureFacebook()
+    {
+        btnFacebook.readPermissions = ["public_profile", "email", "user_friends"];
+        btnFacebook.delegate = self
+    }
+
+
+}
+
